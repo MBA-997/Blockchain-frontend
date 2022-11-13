3f901f87ef75a1f03bc828fcc6f6c09ba9ed6ddd
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-11-13 17:21:10 +0500

    Added Client and Admin page and integrated routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ClientView from "./components/ClientView";
+import AdminView from "./components/AdminView";
+import ButtonAppBar from "./components/AppBar";
+import Home from "./components/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route element={<ButtonAppBar />} /> */}
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <ButtonAppBar />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <>
+              <ButtonAppBar />
+              <AdminView />
+            </>
+          }
+        />
+        <Route
+          path="/client"
+          element={
+            <>
+              <ButtonAppBar />
+              <ClientView />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
