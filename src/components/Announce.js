13ce156b93bbc395a015dcc919bbbc1a46ddc46b
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-11-14 21:02:18 +0500

    Added Notification backend

--- src/components/Announce.js
+++ src/components/Announce.js
@@ -1,18 +1,84 @@
 import React from "react";
-import Logo from "./assets/notification-bell.svg";
-import Announcement from "react-announcement";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: 1.7,
+};
 
 const Announce = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [announce, setAnnounce] = React.useState("");
+
+  const updateAnnounce = (e) => {
+    setAnnounce(e.target.value);
+  };
+
+  const sendAnnounce = () => {
+    localStorage.setItem("announce", JSON.stringify(announce));
+    localStorage.setItem("announceUsed", true);
+  };
+
   return (
-    <Announcement
-      title="Notification: Smart Contract Requirements Updated"
-      subtitle="View details of updates..."
-      link="https://github.com/kristofferandreasen/react-announcement"
-      imageSource={Logo}
-      daysToLive={3}
-      secondsBeforeBannerShows={5}
-      closeIconSize={30}
-    />
+    <div>
+      <Button onClick={handleOpen} variant="contained" color="secondary">
+        <span style={{ color: "white", fontSize: "90%", marginRight: "5%" }}>
+          Create Announcement
+        </span>
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="wallet-model"
+        aria-describedby="connect-meta-mask"
+      >
+        <Box sx={style}>
+          <Stack
+            component="form"
+            sx={{
+              width: "400",
+            }}
+            spacing={2}
+            noValidate
+            autoComplete="off"
+          >
+            <h3
+              style={{
+                color: "grey",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              CREATE ANNOUNCEMENT
+            </h3>
+
+            <TextField
+              id="announcement-id"
+              label="Announcement"
+              variant="filled"
+              required
+              color="secondary"
+              onChange={(e) => updateAnnounce(e)}
+            />
+
+            <Button onClick={() => sendAnnounce()}>Send</Button>
+          </Stack>
+        </Box>
+      </Modal>
+    </div>
   );
 };
 
